720d2c349cc592502da46ab3274263c0ff141054
Fabio Rauber <fabiorauber@gmail.com>
2015-08-25 11:38:55 -0300

    Changes for new Portal Modelo version 1.0rc4 and new collective.recipe.plonesite 1.9

--- manifests/portal.pp
+++ manifests/portal.pp
@@ -1,13 +1,14 @@
 #portal.pp
 
-define pm3::portal ( $instance_name = 'pm3Inst0',
-                     $instance_port = '8100',  
-                     $install_dir   = '/srv/plone',
-                     $smtp_server   = 'localhost',
-                     $smtp_port     = '25',
-                     $email         = 'root@localhost',
-                     $site_title    = 'Portal Modelo 3',
-                     $caching_proxy = 'localhost', 
+define pm3::portal ( $instance_name  = 'pm3Inst0',
+                     $instance_port  = '8100',  
+                     $install_dir    = '/srv/plone',
+                     $smtp_server    = 'localhost',
+                     $smtp_port      = '25',
+                     $email          = 'root@localhost',
+                     $site_title     = 'Portal Modelo 3',
+                     $caching_proxy  = 'localhost',
+                     $upgrade_portal = false,
                     ) {
 
   if !defined (Plone::Site["${name}"]){
@@ -19,12 +20,31 @@ define pm3::portal ( $instance_name = 'pm3Inst0',
       default_language  => 'pt-br',
       instance_name     => $instance_name,
       post_extras       => "${install_dir}/${instance_name}/cfg_site_${name}.py",
-      products          => 'plone.app.caching',
-      profiles_initial  => 'Products.CMFPlone:plone-content',
+      products          => [ 'plone.app.caching',
+                             'plone.resource',
+                             'BrFieldsAndWidgets',
+                             'collective.oembed',
+                             'collective.z3cform.datetimewidget',
+                             'plone.formwidget.datetime',
+                             'plone.formwidget.autocomplete',
+                             'plone.formwidget.contenttree',
+                             'plone.app.dexterity',
+                             'sc.social.like',
+                             'plone.app.event',
+                             'plone.app.intid',
+                             'plone.app.theming',
+                             'interlegis.portalmodelo.buscadores',
+                             'interlegis.portalmodelo.api',
+                             'interlegis.portalmodelo.ombudsman',
+                             'interlegis.portalmodelo.transparency'
+                           ],
+      profiles_initial  => [ 'Products.CMFPlone:plone-content',
+                             'interlegis.portalmodelo.policy:default' 
+                           ],
       refresh_only      => false,
       unless            => "/bin/netstat -tunap|grep 0.0.0.0:${instance_port} && /usr/bin/wget -q -O - http://localhost:${instance_port}/portal/favicon.ico > /dev/null",
-      profiles          => [ 'interlegis.portalmodelo.policy:default',
-                             'plone.app.caching:with-caching-proxy' ]
+      profiles          => 'plone.app.caching:with-caching-proxy',
+      upgrade_portal    => $upgrade_portal, 
     }
 
     file { "${install_dir}/${instance_name}/cfg_site_${name}.py":
