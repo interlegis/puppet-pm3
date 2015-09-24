92a4169439c6af9a2da698e313a4403762411049
Fabio Rauber <fabiorauber@gmail.com>
2015-09-24 14:44:29 -0300

    Added ability to create dbinstances from main class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,10 @@
 # init.pp
 # requires https://github.com/interlegis/puppet-resource-looping.git
 
-class pm3 (  
+class pm3 ( $dbinstances = {}, 
           ) {
 
+validate_hash ($dbinstances)
+create_resources ( "pm3::dbinstance", $dbinstances )
 
 }
